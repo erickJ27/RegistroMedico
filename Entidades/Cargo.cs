acca89473000a57085534b8f1ae0fbb5e2caa182
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-05-21 22:45:54 -0400

    la clase Cargos ya Realizada

---
+++ Entidades/Cargo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Entidades
+{
+    public class Cargos
+    {
+        [Key]
+        public int CargosId { get; set; }
+        public string Descripcion { get; set; }
+
+        public Cargos()
+        {
+            CargosId = 0;
+            Descripcion = string.Empty;
+
+        }
+
+    }
+}
